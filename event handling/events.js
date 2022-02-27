649b9143552f14dd9ecc8afb4d2c3e23c071b2fc
mnnkhndlwl <khandelwalmanan05@gmail.com>
2022-02-27 22:51:36 +0530

    events

---
+++ event handling/events.js
@@ -0,0 +1,18 @@
+//on clicking button our fire function will be called 
+function fire(){
+    document.getElementById('demo').innerHTML=Date() //it would set the content of element with id demo to date of that particular day
+}
+
+function fire2(){
+    document.getElementById('demo').style.color = 'blue';
+}
+
+// Here is a list of some common HTML events:
+
+// Event	                                    Description
+// onchange	                         An HTML element has been changed
+// onclick	                         The user clicks an HTML element
+// onmouseover	                     The user moves the mouse over an HTML element
+// onmouseout	                     The user moves the mouse away from an HTML element
+// onkeydown	                     The user pushes a keyboard key
+// onload	                         The browser has finished loading the page
